c2727517502d19c1166bedd184f6b6ad76ae1be4
xuqihong1998 <413920300@qq.com>
2024-06-23 21:01:26 +0800

    init

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,33 +1,283 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+	"sync"
+	"time"
+)
 
+const maxTaskTime = 10 // seconds
+
+type TaskStat struct {
+	BeginTime int64
+	FileName  string
+	FileIndex int
+	PartIndex int
+	NReduce   int
+	NFiles    int
+}
+
+type TaskQueue struct {
+	queue chan TaskStat
+	mutex sync.Mutex
+}
+
+func NewTaskQueue(len int) TaskQueue {
+	return TaskQueue{
+		queue: make(chan TaskStat, len),
+	}
+}
+
+func (q *TaskQueue) Enqueue(task TaskStat) {
+	q.queue <- task
+}
+
+func (q *TaskQueue) Dequeue() TaskStat {
+	return <-q.queue
+}
+
+func (q *TaskQueue) IsEmpty() bool {
+	return len(q.queue) == 0
+}
+
+func (q *TaskQueue) Size() int {
+	return len(q.queue)
+}
+
+func getNowTimeSecond() int64 {
+	return time.Now().UnixNano() / int64(time.Second)
+}
+
+func (q *TaskQueue) RemoveMapTaskItem(FileIndex int) {
+	q.mutex.Lock()
+	newQueue := make(chan TaskStat, len(q.queue))
+
+	size := len(q.queue)
+
+	for i := 0; i < size; i++ {
+		item := <-q.queue
+		if item.FileIndex != FileIndex {
+			newQueue <- item
+		}
+	}
+	fmt.Printf("the current queue list len is %v\n", len(newQueue))
+	//将新队列中的元素发回原始队列
+	newQueueSize := len(newQueue)
+	for i := 0; i < newQueueSize; i++ {
+		q.queue <- <-newQueue
+	}
+	fmt.Printf("the current q.queue list len is %v\n", len(q.queue))
+	q.mutex.Unlock()
+}
+
+func (q *TaskQueue) RemoveReduceTaskItem(PartIndex int) {
+	q.mutex.Lock()
+	newQueue := make(chan TaskStat, len(q.queue))
+
+	size := len(q.queue)
+
+	for i := 0; i < size; i++ {
+		item := <-q.queue
+		if item.PartIndex != PartIndex {
+			newQueue <- item
+		}
+	}
+
+	newQueueSize := len(newQueue)
+	//将新队列中的元素发回原始队列
+	for i := 0; i < newQueueSize; i++ {
+		q.queue <- <-newQueue
+	}
+	q.mutex.Unlock()
+}
 
 type Coordinator struct {
 	// Your definitions here.
+	// 设置四个队列，代表map reduce任务等待时、运行时的队列
+	mapTaskWaiting    TaskQueue
+	mapTaskRunning    TaskQueue
+	reduceTaskWaiting TaskQueue
+	reduceTaskRunning TaskQueue
 
+	isMapTaskDone bool
+	isDone        bool
+
+	nFile   int
+	nReduce int
+}
+
+// 定义任务分配类型的枚举值
+const (
+	Task_Map = iota
+	Task_Reduce
+	Task_Wait
+	Task_End
+)
+
+type TaskInfo struct {
+	Tasktype  int
+	BeginTime int64
+	FileName  string
+	FileIndex int
+	PartIndex int
+	NReduce   int
+	NFiles    int
+}
+
+func (t *TaskStat) GenerateTaskInfo() TaskInfo {
+	result := TaskInfo{}
+	result.BeginTime = t.BeginTime
+	result.FileName = t.FileName
+	result.FileIndex = t.FileIndex
+	result.PartIndex = t.PartIndex
+	result.NReduce = t.NReduce
+	result.NFiles = t.NFiles
+
+	return result
+}
+
+func (t *TaskStat) SetNow() {
+	t.BeginTime = getNowTimeSecond()
 }
 
 // Your code here -- RPC handlers for the worker to call.
 
-//
 // an example RPC handler.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 	reply.Y = args.X + 1
 	return nil
 }
 
+func (c *Coordinator) distributeReduce() {
+	c.isMapTaskDone = true
+	//初始化reduceTaskWaiting队列
+	for i := 0; i < c.nReduce; i++ {
+		q := TaskStat{}
+		q.FileIndex = i
+		q.PartIndex = i
+		q.NFiles = c.nFile
+		q.NReduce = c.nReduce
+		c.reduceTaskWaiting.Enqueue(q)
+	}
+}
 
-//
+func (c *Coordinator) RemoveTimeOutMapTaskItem() {
+	c.mapTaskRunning.mutex.Lock()
+	newQueue := make(chan TaskStat, len(c.mapTaskRunning.queue))
+
+	size := len(c.mapTaskRunning.queue)
+
+	for i := 0; i < size; i++ {
+		now := getNowTimeSecond()
+		item := <-c.mapTaskRunning.queue
+		if now-item.BeginTime < maxTaskTime {
+			newQueue <- item
+		} else {
+			c.mapTaskWaiting.Enqueue(item)
+		}
+
+	}
+	fmt.Printf("the current queue list len is %v\n", len(newQueue))
+	//将新队列中的元素发回原始队列
+	newQueueSize := len(newQueue)
+	for i := 0; i < newQueueSize; i++ {
+		c.mapTaskRunning.queue <- <-newQueue
+	}
+	fmt.Printf("the current q.queue list len is %v\n", len(c.mapTaskRunning.queue))
+	c.mapTaskRunning.mutex.Unlock()
+}
+
+func (c *Coordinator) RemoveTimeOutReduceTaskItem() {
+	c.reduceTaskRunning.mutex.Lock()
+	newQueue := make(chan TaskStat, len(c.reduceTaskRunning.queue))
+
+	size := len(c.reduceTaskRunning.queue)
+
+	for i := 0; i < size; i++ {
+		now := getNowTimeSecond()
+		item := <-c.reduceTaskRunning.queue
+		if now-item.BeginTime < maxTaskTime {
+			newQueue <- item
+		} else {
+			c.reduceTaskWaiting.Enqueue(item)
+		}
+	}
+
+	newQueueSize := len(newQueue)
+	//将新队列中的元素发回原始队列
+	for i := 0; i < newQueueSize; i++ {
+		c.reduceTaskRunning.queue <- <-newQueue
+	}
+	c.reduceTaskRunning.mutex.Unlock()
+}
+
+func (c *Coordinator) getDistributeReduceType() bool {
+	return c.isMapTaskDone
+}
+
+func (c *Coordinator) AskTask(args *ExampleArgs, reply *TaskInfo) error {
+	if c.mapTaskWaiting.Size() != 0 {
+		//分配map任务
+		task := c.mapTaskWaiting.Dequeue()
+		task.SetNow()
+		c.mapTaskRunning.Enqueue(task)
+		fmt.Printf("input : Map task list num %v\n", c.mapTaskRunning.Size())
+		*reply = task.GenerateTaskInfo()
+		reply.Tasktype = Task_Map
+		fmt.Printf("Distributing map task on %vth file %v\n", reply.FileIndex, reply.FileName)
+		return nil
+	}
+	if c.reduceTaskWaiting.Size() != 0 && c.getDistributeReduceType() {
+		//分配reduce任务
+		task := c.reduceTaskWaiting.Dequeue()
+		task.SetNow()
+		c.reduceTaskRunning.Enqueue(task)
+		*reply = task.GenerateTaskInfo()
+		reply.Tasktype = Task_Reduce
+		fmt.Printf("Distributing reduce task on part %v %vth file %v\n", reply.PartIndex, reply.FileIndex, reply.FileName)
+		return nil
+	}
+	//map 和 reduce任务都分配完了 查看是否还有运行的任务
+	if c.reduceTaskRunning.Size() != 0 || c.mapTaskRunning.Size() != 0 || !c.getDistributeReduceType() {
+		reply.Tasktype = Task_Wait
+		return nil
+	}
+
+	reply.Tasktype = Task_End
+	c.isDone = true
+	return nil
+}
+
+func (c *Coordinator) TaskDone(args *TaskInfo, reply *ExampleReply) error {
+	switch args.Tasktype {
+	case Task_Map:
+		fmt.Printf("Map task on %vth file %v complete\n", args.FileIndex, args.FileName)
+		//这边可能需要根据指定item进行删除
+		c.mapTaskRunning.RemoveMapTaskItem(args.FileIndex)
+		//要在map任务运行完成后才执行后续的reduce任务
+		fmt.Printf("Out : Map task list num %v\n", c.mapTaskRunning.Size())
+		if c.mapTaskRunning.Size() == 0 && c.mapTaskWaiting.Size() == 0 {
+			c.distributeReduce()
+		}
+		break
+	case Task_Reduce:
+		fmt.Printf("Reduce task on %vth part complete\n", args.PartIndex)
+		c.reduceTaskRunning.RemoveReduceTaskItem(args.PartIndex)
+		break
+	default:
+		panic("Task Done error")
+	}
+	return nil
+}
+
 // start a thread that listens for RPCs from worker.go
-//
 func (c *Coordinator) server() {
 	rpc.Register(c)
 	rpc.HandleHTTP()
@@ -41,29 +291,59 @@ func (c *Coordinator) server() {
 	go http.Serve(l, nil)
 }
 
-//
 // main/mrcoordinator.go calls Done() periodically to find out
 // if the entire job has finished.
-//
 func (c *Coordinator) Done() bool {
 	ret := false
 
 	// Your code here.
-
+	ret = c.isDone
 
 	return ret
 }
 
-//
+func (c *Coordinator) removeTimeOutTask() {
+	c.RemoveTimeOutMapTaskItem()
+	c.RemoveTimeOutReduceTaskItem()
+
+}
+func (c *Coordinator) loopRemoveTimeOut() {
+	for true {
+		time.Sleep(2 * 1000 * time.Millisecond)
+		c.removeTimeOutTask()
+	}
+}
+
 // create a Coordinator.
 // main/mrcoordinator.go calls this function.
 // nReduce is the number of reduce tasks to use.
-//
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
 	c := Coordinator{}
 
-	// Your code here.
+	c.isDone = false
+	c.isMapTaskDone = false
 
+	c.mapTaskWaiting = NewTaskQueue(len(files))
+	c.mapTaskRunning = NewTaskQueue(len(files))
+	c.reduceTaskRunning = NewTaskQueue(nReduce)
+	c.reduceTaskWaiting = NewTaskQueue(nReduce)
+
+	log.Printf("the file len is %v\n", len(files))
+	// Your code here.
+	c.nFile = len(files)
+	c.nReduce = nReduce
+
+	//start a thread to handle the timeout task
+	go c.loopRemoveTimeOut()
+
+	for i := 0; i < len(files); i++ {
+		q := TaskStat{}
+		q.FileName = files[i]
+		q.FileIndex = i
+		q.NReduce = nReduce
+		log.Printf("input taskstat FileName %v\n", q.FileName)
+		c.mapTaskWaiting.Enqueue(q)
+	}
 
 	c.server()
 	return &c
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,36 +1,177 @@
 package mr
 
-import "fmt"
-import "log"
-import "net/rpc"
-import "hash/fnv"
+import (
+	"encoding/json"
+	"fmt"
+	"hash/fnv"
+	"io/ioutil"
+	"log"
+	"net/rpc"
+	"os"
+	"path/filepath"
+	"sort"
+	"strconv"
+	"time"
+)
 
+type ByKey []KeyValue
 
-//
+// for sorting by key.
+func (a ByKey) Len() int           { return len(a) }
+func (a ByKey) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByKey) Less(i, j int) bool { return a[i].Key < a[j].Key }
+
 // Map functions return a slice of KeyValue.
-//
 type KeyValue struct {
 	Key   string
 	Value string
 }
 
-//
 // use ihash(key) % NReduce to choose the reduce
 // task number for each KeyValue emitted by Map.
-//
 func ihash(key string) int {
 	h := fnv.New32a()
 	h.Write([]byte(key))
 	return int(h.Sum32() & 0x7fffffff)
 }
 
+func CallTaskDone(taskInfo *TaskInfo) {
+	reply := ExampleReply{}
+	call("Coordinator.TaskDone", taskInfo, &reply)
+}
 
-//
 // main/mrworker.go calls this function.
-//
 func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
+	for {
+		taskInfo := CallAskTask()
+		log.Printf("taskInfo receive, the fileIndex is %v\n", taskInfo.FileIndex)
+		switch taskInfo.Tasktype {
+		case Task_Map:
+			outprefix := "../mr-tmp/mr-" + strconv.Itoa(taskInfo.FileIndex) + "/mr-"
+			intermediate := []KeyValue{}
+			file, err := os.Open(taskInfo.FileName)
+			if err != nil {
+				log.Fatalf("cannot open %v", taskInfo.FileName)
+			}
+			content, err := ioutil.ReadAll(file)
+			if err != nil {
+				log.Fatalf("cannot read %v", taskInfo.FileName)
+			}
+			file.Close()
+			kva := mapf(taskInfo.FileName, string(content))
+			intermediate = append(intermediate, kva...)
+			//定义nReduce 创建输出文件
+			nReduce := taskInfo.NReduce
+			outFiles := make([]*os.File, nReduce)
+			fileEncs := make([]*json.Encoder, nReduce)
 
+			// 创建临时目录
+			tmpDir := "../mr-tmp/mr-" + strconv.Itoa(taskInfo.FileIndex)
+			err = os.MkdirAll(tmpDir, 0755)
+			if err != nil {
+				fmt.Printf("Failed to create temp directory: %v\n", err)
+				return
+			}
+
+			for outindex := 0; outindex < nReduce; outindex++ {
+				outdir := "../mr-tmp/mr-" + strconv.Itoa(taskInfo.FileIndex)
+				outFiles[outindex], err = os.CreateTemp(outdir, "mr-tmp-*")
+				fileEncs[outindex] = json.NewEncoder(outFiles[outindex])
+				if err != nil {
+					fmt.Printf("Failed to create temp file: %v\n", err)
+				}
+			}
+			//将kv输入本地暂时文件
+			for _, kv := range intermediate {
+				outindex := ihash(kv.Key) % nReduce
+				file = outFiles[outindex]
+				enc := fileEncs[outindex]
+				err := enc.Encode(&kv)
+				if err != nil {
+					fmt.Printf("File %v Key %v value %v Error: %v\n", taskInfo.FileName, kv.Key, kv.Value, err)
+					panic("Json encode failed")
+				}
+			}
+
+			//保存文件
+			for outindex, file := range outFiles {
+				outname := outprefix + strconv.Itoa(outindex)
+				oldpath := filepath.Join(file.Name())
+				os.Rename(oldpath, outname)
+				file.Close()
+			}
+
+			taskInfo.Tasktype = Task_Map
+
+			CallTaskDone(taskInfo)
+			break
+		case Task_Reduce:
+			//to do
+			outname := fmt.Sprintf("mr-out-%v", taskInfo.PartIndex)
+
+			innameprefix := "../mr-tmp/mr-"
+			//innamesuffix := "-" + strconv.Itoa(taskInfo.PartIndex)
+			//读取文件为键值对
+			intermediate := []KeyValue{}
+			for index := 0; index < taskInfo.NFiles; index++ {
+				inname := innameprefix + strconv.Itoa(index) + "/mr-" + strconv.Itoa(taskInfo.PartIndex)
+				file, err := os.Open(inname)
+				if err != nil {
+					fmt.Printf("Open intermediate file %v failed: %v\n", inname, err)
+					panic("Open file error")
+				}
+				dec := json.NewDecoder(file)
+				for {
+					var kv KeyValue
+					if err := dec.Decode(&kv); err != nil {
+						break
+					}
+					intermediate = append(intermediate, kv)
+				}
+				file.Close()
+			}
+			sort.Sort(ByKey(intermediate))
+			ofile, err := os.CreateTemp("../mr-tmp", "mr-*")
+			if err != nil {
+				fmt.Printf("Create output file failed: %v\n", err)
+				panic("Create file error")
+			}
+			i := 0
+			for i < len(intermediate) {
+				j := i + 1
+				for j < len(intermediate) && intermediate[j].Key == intermediate[i].Key {
+					j++
+				}
+				values := []string{}
+				for k := i; k < j; k++ {
+					values = append(values, intermediate[k].Value)
+				}
+				output := reducef(intermediate[i].Key, values)
+
+				// 输出文件
+				fmt.Fprintf(ofile, "%v %v\n", intermediate[i].Key, output)
+
+				i = j
+			}
+
+			os.Rename(filepath.Join(ofile.Name()), outname)
+			ofile.Close()
+			taskInfo.Tasktype = Task_Reduce
+			CallTaskDone(taskInfo)
+
+			break
+		case Task_Wait:
+			time.Sleep(time.Duration(time.Second * 5))
+			break
+		case Task_End:
+			fmt.Println("Master all tasks complete. Nothing to do...")
+			return
+		default:
+			panic("Invalid Task state received by worker")
+		}
+
+	}
 	// Your worker implementation here.
 
 	// uncomment to send the Example RPC to the coordinator.
@@ -38,11 +179,9 @@ func Worker(mapf func(string, string) []KeyValue,
 
 }
 
-//
 // example function to show how to make an RPC call to the coordinator.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
 func CallExample() {
 
 	// declare an argument structure.
@@ -67,11 +206,9 @@ func CallExample() {
 	}
 }
 
-//
 // send an RPC request to the coordinator, wait for the response.
 // usually returns true.
 // returns false if something goes wrong.
-//
 func call(rpcname string, args interface{}, reply interface{}) bool {
 	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
 	sockname := coordinatorSock()
@@ -88,4 +225,18 @@ func call(rpcname string, args interface{}, reply interface{}) bool {
 
 	fmt.Println(err)
 	return false
+}
+
+func CallAskTask() *TaskInfo {
+	args := ExampleArgs{}
+	reply := TaskInfo{}
+	ok := call("Coordinator.AskTask", &args, &reply)
+	if ok {
+		// reply.Y should be 100.
+		log.Printf("call success, the fileName is %v, the task type is %v\n", reply.FileName, reply.Tasktype)
+	} else {
+		log.Printf("call failed!\n")
+	}
+
+	return &reply
 }
